6a2ea8dcbee7ca6ec46f0f1b11078b5fef1dfda8
Bartek Jakoniuk <ba.jakoniuk@gmail.com>
2018-09-13 07:50:45 +0200

    Lint scss with stylelint

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,32 @@
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var jshint = require('gulp-jshint');
+var scsslint = require('gulp-scss-lint');
 
-gulp.task('sass', function() {
-    return gulp.src('scss/main.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            errLogToConsole: true,
-            outputStyle: 'expanded'
-        }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css'))
+gulp.task('sass',['scss-lint'], function() {
+  return gulp.src('scss/main.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true,
+      outputStyle: 'expanded'
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('css'));
 });
-
 
 gulp.task('watch', function(){
-    gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('js', function(){
-    return gulp.scr('gulpfile.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter('default'));
+gulp.task('js-lint', function(){
+  return gulp.src('js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 
 });
 
+gulp.task('scss-lint', function () {
+  return gulp.src('scss/**/*.scss')
+    .pipe(scsslint());
+});
